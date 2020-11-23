157c140101f16231dd6790f2044dbbbb54dbd6fe
mac <ahnset24@gmail.com>
2020-11-23 13:07:25 +0000

    Adding analytics tracking features.

--- app/src/main/java/com/icgen/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/icgen/retirementcalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +13,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         appCenterSetup()
+
+        onCalculateButtonClicked()
     }
+
+    private fun onCalculateButtonClicked() {
+        calculateButton.setOnClickListener {
+//            throw Exception("Something wrong happened")
+//            Crashes.generateTestCrash()
+
+            trackInterestRateAnalytics()
+            trackRetirementAgeAnalytics()
+        }
+    }
+
+    private fun trackInterestRateAnalytics() {
+        val interestRate = interestEditText.text.toString().toFloat()
+        if (interestRate <= 0) Analytics.trackEvent("wrong_interest_rate")
+    }
+
+    private fun trackRetirementAgeAnalytics() {
+        val currentAge = getCurrentAge()
+        val retirementAge = getRetirementAge()
+        if (retirementAge <= currentAge) Analytics.trackEvent("wrong_age")
+    }
+
+    private fun getCurrentAge() = ageEditText.text.toString().toInt()
+    private fun getRetirementAge() = retirementEditText.text.toString().toInt()
 
     private fun appCenterSetup() {
         AppCenter.start(
